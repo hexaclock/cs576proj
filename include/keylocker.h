d4df56a69efcfe6a5f0099855e07d6f1c9c6ec99
Daniel Vinakovsky <dvinakov@stevens.edu>
2015-10-11 16:32:00 -0400

    updated include files

--- include/keylocker.h
+++ include/keylocker.h
@@ -1,5 +1,9 @@
 #ifndef _KEYLOCKER_H_
-
 #define _KEYLOCKER_H_
+
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <linux/limits.h>
 
 #endif
--- include/network.h
+++ include/network.h
@@ -1,15 +1,12 @@
 #ifndef _NETWORK_H_
-
 #define _NETWORK_H_
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <signal.h>
-
-#ifdef _CLIENT_
 #include <netdb.h>
-#endif
 
 #endif
