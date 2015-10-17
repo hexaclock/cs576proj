61ad9b8b6172e11088b0b4a6dc7f5c8cdeebee4b
Bradford Smith <bsmith8@stevens.edu>
2015-10-17 16:38:06 -0400

    Added documentation and closed files

--- src/JsonParsing.cpp
+++ src/JsonParsing.cpp
@@ -2,8 +2,11 @@
 #include "keylocker.h"
 #include "json/json.h"
 
+//change this to a 0 for fast non-readable json output
 #define READABLE 1
 
+/* class for reading and writing to a JSON database file
+ */
 class JsonParsing
 {
     public:
@@ -11,6 +14,11 @@ class JsonParsing
         bool writeJson(Json::Value* root, std::string dbName);
 };
 
+/* pre: takes in a Json::Value* root and std::string dbName
+ * post: reads the data in the Json::Value pointed to by root into a file called
+ *      dbName
+ * returns: true on success, false otherwise
+ */
 bool readJson(Json::Value* root, std::string dbName)
 {
     Json::Reader reader;
@@ -23,12 +31,19 @@ bool readJson(Json::Value* root, std::string dbName)
     if (!reader.parse(passdb_file, *root, false))
     {
         std::cout  << reader.getFormattedErrorMessages() << std::endl;
+        passdb_file.close();
         return false;
     }
 
+    passdb_file.close();
     return true;
 }
 
+/* pre: takes in a Json::Value* root and std::string dbName
+ * post: writes the data in the Json::Value pointed to by root into a file
+ *      called dbName
+ * returns: true on success, false otherwise
+ */
 bool writeJson(Json::Value* root, std::string dbName)
 {
 #ifdef READABLE
@@ -42,7 +57,10 @@ bool writeJson(Json::Value* root, std::string dbName)
     if (!outFile.is_open())
         return false;
 
+    //maybe we need to error check this later?
     outFile << writer.write(*root);
+
+    outFile.close();
 
     return true;
 }
