efe4aac7d3a18476a7e12c93b8490f194494d051
Bradford Smith <bsmith8@stevens.edu>
2015-10-17 15:50:58 -0400

    Json stuff
    halp not working

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,5 +1,6 @@
 #include "network.h"
 #include "keylocker.h"
+#include "json/json.h"
 
 class Client
 {
@@ -24,8 +25,22 @@ int main(int argc, char **argv)
     if (argc < 2)
         panic("usage: " + (std::string)argv[0] + " <username>", 1);
 
-    //std::fstream db;
-    //db.open();
+    Json::Value root;
+    Json::Reader reader;
 
+    std::fstream db;
+    std::cout << (std::string)argv[1] + ".db" << std::endl;
+    db.open((std::string)argv[1] + ".db", std::fstream::binary);
+    reader.parse(db, root, false);
+    /*
+    if (!reader.parse(db, root, false))
+    {
+        panic("Failed to parse Json!", 2);
+    }
+    */
+
+    std::cout << root;
+
+    db.close();
     return 0;
 }
