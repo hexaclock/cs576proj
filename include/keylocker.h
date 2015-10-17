62e652e11702bccd36d0eafd25836917de2cdfa8
Bradford Smith <bsmith8@stevens.edu>
2015-10-17 15:19:02 -0400

    Fixed JSONCPP stuff
    Also added a few includes

--- include/keylocker.h
+++ include/keylocker.h
@@ -1,6 +1,8 @@
 #ifndef _KEYLOCKER_H_
 #define _KEYLOCKER_H_
 
+#include <fstream>
+#include <iostream>
 #include <openssl/bio.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
--- src/Client.cpp
+++ src/Client.cpp
@@ -3,25 +3,29 @@
 
 class Client
 {
-public:
-  int connect(std::string servname);
-  bool auth();
-  std::string newEntry();
-  bool editEntry(std::string euuid);
-  bool deleteEntry(std::string euuid);
-private:
-  
+    public:
+        int connect(std::string servname);
+        bool auth();
+        std::string newEntry();
+        bool editEntry(std::string euuid);
+        bool deleteEntry(std::string euuid);
+    private:
+
 };
 
 void panic(std::string msg, int code)
 {
-  std::cout << msg << std::endl;
-  exit(code);
+    std::cout << msg << std::endl;
+    exit(code);
 }
 
 int main(int argc, char **argv)
 {
-  if (argc < 2)
-    panic("usage: "+argv[0]+" <server hostname>",1);
-  return 0;
+    if (argc < 2)
+        panic("usage: " + (std::string)argv[0] + " <username>", 1);
+
+    //std::fstream db;
+    //db.open();
+
+    return 0;
 }
