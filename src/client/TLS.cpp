f89a88994fd9de8447a1eba4ba2cabdb23e7b904
chx383141801 <383141801@qq.com>
2015-11-10 18:22:57 -0500

    Implement regist, upload, download functions

--- src/client/TLS.cpp
+++ src/client/TLS.cpp
@@ -1,5 +1,101 @@
 #include "network.h"
 
+#define MAXDATASIZE 20480		/*20k bytes*/
+
+int regist (WOLFSSL* ssl, std::string &data) 
+{
+	char sendBuf[MAXDATASIZE], rcvBuf[MAXDATASIZE] = {0};
+	int ret = 0;
+
+	strcpy(sendBuf, data.c_str());
+	if (wolfSSL_write(ssl, sendBuf, strlen(sendBuf)) != strlen(sendBuf))
+	{
+		ret = wolfSSL_get_error(ssl, 0);
+		std::cout << "WolfSSL write error. Error: " << ret << std::endl;
+		return -1;
+	}
+
+	if (wolfSSL_read(ssl, rcvBuf, MAXDATASIZE) < 0)
+	{
+		ret = wolfSSL_get_error(ssl, 0);
+		std::cout << "WolfSSL read error. Error: " << ret << std::endl;
+	}
+	std::cout << "Recieved: " << rcvBuf << std::endl;
+	
+	return ret;
+}
+
+int upload (WOLFSSL* ssl, std::string &data)
+{
+	char sendBuf[MAXDATASIZE], rcvBuf[MAXDATASIZE] = {0};
+	int ret = 0;
+
+	strcpy(sendBuf, data.c_str());
+
+	if (wolfSSL_write(ssl, sendBuf, strlen(sendBuf)) != strlen(sendBuf))
+	{
+		ret = wolfSSL_get_error(ssl, 0);
+		std::cout << "WolfSSL write error. Error: " << ret << std::endl;
+		return -1;
+	}
+
+	if (wolfSSL_read(ssl, rcvBuf, MAXDATASIZE) < 0)
+	{
+		ret = wolfSSL_get_error(ssl, 0);
+		std::cout << "WolfSSL read error. Error: " << ret << std::endl;
+	}
+	std::cout << "Recieved: " << rcvBuf << std::endl;
+
+	return ret;
+}
+
+/* pre:  pass request data; pass the path of database (~/.keyloker)
+ * post: update the database file with the one from server.
+ * return: true on success; false on failed
+ */
+bool download (WOLFSSL* ssl, std::string &data, std::string &dbpath)
+{
+	char sendBuf[MAXDATASIZE], rcvBuf[MAXDATASIZE] = {0};
+	int ret;
+	std::string username;
+	std::string msg;
+	std::string filename;
+	std::string delimiter = ":";
+    std::string b64dat;
+
+    /*parse the data for file update*/
+	msg.erase(0, msg.find(delimiter) + delimiter.length());
+	username = msg.substr(0, msg.find(delimiter));
+	filename = username + "_keylocker.db";
+	std::ofstream fout(dbpath + '/' + filename);
+
+	strcpy(sendBuf, data.c_str());
+
+	if (wolfSSL_write(ssl, sendBuf, strlen(sendBuf)) != strlen(sendBuf))
+	{
+		ret = wolfSSL_get_error(ssl, 0);
+		std::cout << "WolfSSL write error. Error: " << ret << std::endl;
+		return false;
+	}
+
+	if (wolfSSL_read(ssl, rcvBuf, MAXDATASIZE) < 0)
+	{
+		ret = wolfSSL_get_error(ssl, 0);
+		std::cout << "WolfSSL read error. Error: " << ret << std::endl;
+	}
+	
+	std::cout << "Message recieved" <<std::endl;
+	std::string rcv= rcvBuf;
+    b64dat = rcv.substr(rcv.find_last_of(delimiter) + 1);
+
+	if (!fout.is_open())
+		return false;
+
+	fout << b64dat;
+	fout.close();
+	return true;
+}
+
 int tls_send(std::string &hostname, int portnum,
              std::string &data)
 {
@@ -70,6 +166,8 @@ int tls_send(std::string &hostname, int portnum,
          * DOWNLOAD:username:secretkey\n
          *
          */
+		if (data contains upload)
+				upload(wssl,
         return 0;
         
     }
